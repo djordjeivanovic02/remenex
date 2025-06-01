fcb1551dc3721c44acdca68e4b9ce93a57ebd48f
lekeee <jaleksa388@gmail.com>
2025-06-01 09:49:30 +0200

    domain change for profa

--- app/components/timeline.tsx
+++ app/components/timeline.tsx
@@ -224,7 +224,7 @@ export function TimelineDemo() {
                 </div>
               </div>
             </Link>
-            <Link href="https://profatest.netlify.app/" target="_blank">
+            <Link href="https://profa.app/" target="_blank">
               <div className="relative group h-20 md:h-44 lg:h-60 w-full overflow-hidden rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]">
                 <Image
                   src="/images/profa.png"
